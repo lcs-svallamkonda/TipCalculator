167081261ef3b69810f2029b67010c30871e784b
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-10-13 13:30:37 -0400

    Initial Commit

---
+++ TipCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculator
+//
+//  Created by Vallamkonda, Sunaina on 2019-10-13.
+//  Copyright © 2019 Vallamkonda, Sunaina. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
