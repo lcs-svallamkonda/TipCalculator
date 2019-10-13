ab86b0f80c9e4a998fbc82fb094ea310931aeb46
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-10-13 15:20:35 -0400

    Figured out code in playground

--- TipCalculator/TipCalculator.playground/Contents.swift
+++ TipCalculator/TipCalculator.playground/Contents.swift
@@ -1,3 +1,26 @@
 import UIKit
 
+// Text Fields
+let billAmount = UITextField()
+let tipPercentage = UITextField()
+let splitBetweenHowManyPeople = UITextField()
 
+// Simulate user input
+billAmount.text = "100"
+tipPercentage.text = "15"
+splitBetweenHowManyPeople.text = "2"
+
+// Get actual strings from each text field
+//Force unwrap the optional values
+let billAmountAsString = billAmount.text!
+let tipPercentageAsString = tipPercentage.text!
+let splitBetweenHowManyPeopleAsString = splitBetweenHowManyPeople.text!
+
+//Turn strings into Double Values
+let billAmountAsDouble = Double(billAmountAsString)!
+let tipPercentageAsDouble = Double(tipPercentageAsString)!
+let splitBetweenHowManyPeopleAsDouble = Double(splitBetweenHowManyPeopleAsString)!
+
+//Calculate the tip
+let tipAmountInDollars = billAmountAsDouble*tipPercentageAsDouble/100
+let tipAmountPerPerson = tipAmountInDollars/splitBetweenHowManyPeopleAsDouble
