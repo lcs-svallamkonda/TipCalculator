2eda6b6552cadd2ef3001dad2770bc50c78e7c88
Sunaina Vallamkonda <svallamkonda@lcs.on.ca>
2019-10-16 18:55:24 -0400

    Finished 1 attempt at code

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,9 +10,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: Properties
+    @IBOutlet weak var billAmount: UITextField!
+    @IBOutlet weak var tipPercentage: UITextField!
+    @IBOutlet weak var splitBetweenThisManyPeople: UITextField!
+    
+    //MARK: Initializers
+    
+    
+    //MARK: Methods (functions) - behaviours
+    
+    //runs as soon as the view becomes visible to the user
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+       
+        // Get actual strings from each text field
+        //Force unwrap the optional values
+        let billAmountAsString = billAmount.text!
+        let tipPercentageAsString = tipPercentage.text!
+        let splitBetweenThisManyPeopleAsString = splitBetweenThisManyPeople.text!
+        
+        //Turn strings into Double Values
+        let billAmountAsDouble = Double(billAmountAsString)!
+        let tipPercentageAsDouble = Double(tipPercentageAsString)!
+        let splitBetweenThisManyPeopleAsDouble = Double(splitBetweenThisManyPeopleAsString)!
+        
+        //Turn tip percentage into a percent
+        let tipPercentageAsPercent = tipPercentageAsDouble/100
+        
+        //Calculate the Tip
+        let tipAmountInDollars = billAmountAsDouble*tipPercentageAsPercent
+        let tipAmountPerPerson = tipAmountInDollars/splitBetweenThisManyPeopleAsDouble
     }
 
 
